4ffd0f6f749a33bfc78b3724d1174ae738512ef6
hasan330 <hasan.saleh.khader@gmail.com>
2018-04-23 10:51:29 +0300

    Cleaning up app.js from project

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { createStore, applyMiddleware } from 'redux';
 
 import {BrowserRouter, Route } from 'react-router-dom'
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -28,10 +27,8 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
    <BrowserRouter>
    	<div>
-   		Header
-   		<Route path="/hello"  component={Hello} />
-   		<Route path="/goodbye"  component={Goodbye}/>
-   	  </div>
+   
+   	</div>
    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
