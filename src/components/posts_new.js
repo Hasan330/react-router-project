7759a1e9c73aabf035c43b8c969adfa9ed1ca597
hasan330 <hasan.saleh.khader@gmail.com>
2018-04-24 10:11:32 +0300

    Add new route

---
+++ src/components/posts_new.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+
+	render(){
+		return(
+			<div>
+				Posts New
+			</div>
+		)
+	
+	}
+}
+
+export default PostsNew;
