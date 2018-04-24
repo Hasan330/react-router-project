f8247297cd6d19abcde4d9e11fa3dff3b9be355f
hasan330 <hasan.saleh.khader@gmail.com>
2018-04-24 15:15:10 +0300

    Ternary operation to show form validation only after the field has been touched

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,7 +11,7 @@ class PostsNew extends Component {
 					className='form-control'
 					{...field.input}
 				/>
-				{field.meta.error}
+				{field.meta.touched ? field.meta.error : ''}
 			</div>
 		)
 	}
@@ -23,8 +23,6 @@ class PostsNew extends Component {
 
 	render(){
 		const { handleSubmit } = this.props;
-
-
 
 		return(
 			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
