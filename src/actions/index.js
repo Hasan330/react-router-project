e026b1d1f32f2d437adb5d5b88b97a72886772b7
hasan330 <hasan.saleh.khader@gmail.com>
2018-04-25 09:59:19 +0300

    Create action and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -9,7 +10,6 @@ const API_KEY = '?key=hasan123';
 export function fetchPosts() {
 
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-
 	console.log("Axios request is: ", request);
 
 	return {
@@ -27,4 +27,15 @@ export function createPost(values, callback) {
 		payload: request
 	}
 
+}
+
+export function fetchPost(id) {
+
+	const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+	console.log("Axios request is: ", request);
+
+	return {
+		type: FETCH_POST,
+		payload: request
+	}
 }
